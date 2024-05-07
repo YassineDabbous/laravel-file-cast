5100d7cbf742a068986c2438ad298de58f3642c4
yassine-dabbous <yassine.dabbous@gmail.com>
2024-05-07 01:12:53 +0100

    custom disk per column

--- src/FileCast.php
+++ src/FileCast.php
@@ -4,6 +4,7 @@ namespace Yaseen\FileCast;
 
 use Illuminate\Support\Facades\Storage;
 use Illuminate\Contracts\Database\Eloquent\CastsAttributes;
+use Illuminate\Database\Eloquent\Model;
 
 class FileCast implements CastsAttributes
 {
@@ -12,7 +13,7 @@ class FileCast implements CastsAttributes
         $this->disk = config('file-cast.disk', 'public');
     }
 
-    public function get($model, string $key, $value, array $attributes)
+    public function get(Model $model, string $key, mixed $value, array $attributes)
     {
         return $value;
     }
@@ -20,22 +21,42 @@ class FileCast implements CastsAttributes
     /**
      * @param  \Illuminate\Http\UploadedFile|null  $value
      */
-    public function set($model, string $key, $value, array $attributes)
+    public function set(Model $model, string $key, mixed $value, array $attributes)
     {
         if (is_file($value)) {
+            $this->disk = $this->getDisk($model, $key);
+            
             // delete old file if exists
             if (isset($attributes[$key])) {
                 if (Storage::disk($this->disk)->exists($attributes[$key])) {
                     Storage::disk($this->disk)->delete($attributes[$key]);
                 }
             }
+
             $value = $value->store(
                 config('file-cast.folder') ?? $model?->getTable() ?? 'file_cast_default_path',
                 ['disk' => $this->disk]
             );
             return $value;
-        } else {
-            return $value;
         }
+        
+        return $value;
+    }
+
+
+    public function getDisk(Model $model, string $key){
+        $disks = [];
+        if(method_exists($model, 'disks')){
+            $disks = $model->disks();
+        }
+        else if(property_exists($model, 'disks')){
+            $disks = $model->disks;
+        }
+
+        if(isset($disks[$key])){
+            return $disks[$key];
+        }
+
+        return $this->disk;
     }
 }
