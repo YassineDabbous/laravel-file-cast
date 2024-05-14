3344ee91ae8ad9a15c3aff8ec3a6f4f55add6658
yassine-dabbous <yassine.dabbous@gmail.com>
2024-05-14 12:33:38 +0100

    use env

--- src/config.php
+++ src/config.php
@@ -2,13 +2,13 @@
 
 return [
     /** Default storage disk */
-    'disk' => 'public',
+    'disk' => env('FILE_CAST_DISK', 'public'),
 
     /**
      * Default storage folder. If NULL, the Model table name will be used.
      */
-    'folder' => NULL,
+    'folder' => env('FILE_CAST_FOLDER', NULL),
 
     /** Automatically delete old files */
-    'auto_delete' => true,
+    'auto_delete' => env('FILE_CAST_AUTO_DELETE', true),
 ];
